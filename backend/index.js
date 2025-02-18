5740a204286232edffcb93639d7f4d51269b6cc5
Kshitij Akarsh <kshitijakarsh@Kshitijs-MacBook-Air.local>
2025-02-18 15:28:54 +0530

    database added

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,29 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./.env" });
+
 const cors = require("cors");
 const userRoutes = require("./routes/userRoutes");
-const gymRoutes = require("./routes/gymRoutes")
-const gymDashboardRoutes = require("./routes/gymDashboardRoutes")
+const gymRoutes = require("./routes/gymRoutes");
+const gymDashboardRoutes = require("./routes/gymDashboardRoutes");
 
 const app = express();
 const PORT = 3000;
 
+const DB = process.env.MONGO_URL;
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("mongodb://localhost:27017/");
+mongoose
+  .connect(DB, {})
+  .then(() => {
+    console.log("Db connected successfully");
+  })
+  .catch((err) => {
+    console.log(`Error while connecting to database: ${err}`);
+  });
 
 app.use("/api/users", userRoutes);
 app.use("/api/gyms", gymRoutes);
 app.use("/api/gym-dashboard", gymDashboardRoutes);
-
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
